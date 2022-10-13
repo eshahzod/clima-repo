d7b3a8af232dc2f8596564db48e17ea4545c4a1f
eshahzod <8shahzodbekerkinov@gmail.com>
2022-10-13 16:37:43 +0500

    initial commit

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:clima/screens/city_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 import 'package:clima/services/weather.dart';
@@ -57,17 +57,28 @@ class _LocationScreenState extends State<LocationScreen> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   TextButton(
-                    onPressed: () {
-                      var weatherData = weatherModel.getLocationWeather();
-                      updateUI(weatherData);
-                    },
+                    onPressed: () {},
                     child: Icon(
                       Icons.near_me,
                       size: 50.0,
                     ),
                   ),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      var typedName = await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: ((context) {
+                            return CityScreen();
+                          }),
+                        ),
+                      );
+                      if (typedName != null) {
+                        var weatherData =
+                            await weatherModel.getCityLocation(typedName);
+                        updateUI(weatherData);
+                      }
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,
--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -4,12 +4,22 @@ import 'package:clima/services/networking.dart';
 const String apiKey = '8e4efa172fe81797e059b60c5e843ad6';
 
 class WeatherModel {
+  Future getCityLocation(String cityName) async {
+    var url =
+        'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper = NetworkHelper(url);
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future getLocationWeather() async {
     Location location = Location();
     await location.getCurrentLocation();
     NetworkHelper networkHelper = NetworkHelper(
         'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
     var weatherData = await networkHelper.getData();
+    print(weatherData);
 
     return weatherData;
   }
